0caf0294df80cc05b940a71ad3a5e045cd2a98e1
BuildTools <unconfigured@null.spigotmc.org>
2021-03-02 02:24:31 +0100

    updates

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -60,9 +60,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'Viking Swap')}
+          {TranslateString(576, 'Wasabi Finance')}
         </Heading>
-        <Text>{TranslateString(578, 'The only yield farm for Warriors on the journey to Valhalla on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'The yield farm for Wasabi on the Binance Smart Chain.')}</Text>
       </Hero>
       <div>
         <Cards>
--- src/views/Home/components/CakeStats.tsx
+++ src/views/Home/components/CakeStats.tsx
@@ -41,10 +41,10 @@ const CakeStats = () => {
     <StyledCakeStats>
       <CardBody>
         <Heading size="xl" mb="24px">
-          {TranslateString(534, 'Egg Stats')}
+          {TranslateString(534, 'WASABI Stats')}
         </Heading>
         <Row>
-          <Text fontSize="14px">{TranslateString(536, 'Total VIKING Supply')}</Text>
+          <Text fontSize="14px">{TranslateString(536, 'Total WASABI Supply')}</Text>
           {cakeSupply && <CardValue fontSize="14px" value={cakeSupply} decimals={0} />}
         </Row>
         <Row>
@@ -52,11 +52,11 @@ const CakeStats = () => {
           <CardValue fontSize="14px" value={getBalanceNumber(marketCap)} decimals={0} prefix="$" />
         </Row>
         <Row>
-          <Text fontSize="14px">{TranslateString(538, 'Total VIKING Burned')}</Text>
+          <Text fontSize="14px">{TranslateString(538, 'Total WASABI Burned')}</Text>
           <CardValue fontSize="14px" value={getBalanceNumber(burnedBalance)} decimals={0} />
         </Row>
         <Row>
-          <Text fontSize="14px">{TranslateString(540, 'New EGG/block')}</Text>
+          <Text fontSize="14px">{TranslateString(540, 'New WASABI/block')}</Text>
           <Text bold fontSize="14px">{vikingPerBlock}</Text>
         </Row>
       </CardBody>
